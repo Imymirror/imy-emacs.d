a2dcfbbf9ff86e670e4ebf5c8c3e4baae541267d
imymirror <2633610394@qq.com>
2021-12-07 21:43:05 +0800

    vanilla emacs

---
+++ .vanilla-emacs.d/early-init.el
@@ -0,0 +1,4 @@
+;;  -*- lexical-binding: t; -*-
+
+
+(provide 'early-init)
---
+++ .vanilla-emacs.d/lisp/init-mode-enable.el
@@ -0,0 +1,10 @@
+;;  -*- lexical-binding: t; -*-
+
+(require 'winum)
+(winum-mode)
+(delete-selection-mode)
+(doom-modeline-mode)
+(org-bullets-mode)
+(global-company-mode)
+
+(provide 'init-mode-enable)
