e13445ed56eb97170d5cc8c66c00ff938592303e
imymirror <2633610394@qq.com>
2022-01-24 07:39:48 +0800

    backup

--- .vanilla-emacs.d/lisp/init-blog.el
+++ .vanilla-emacs.d/lisp/init-blog.el
@@ -11,17 +11,17 @@
   "run hugo server and open preview in browser"
   (interactive)
   (shell-command-to-string "kill -9 $(pgrep -f 'hugo server')")
-  (start-process "my-process" "blog-export" "sh" "/Users/fuhongxue/.emacs.d/shell/blog-preview.sh" "/bin")
+  (start-process "my-process" "automation" "sh" "/Users/fuhongxue/.emacs.d/shell/blog-preview.sh" "/bin")
 (run-with-timer 1 nil (lambda () (browse-url "http://localhost:1313"))))
 
 (defun imi/blog-publish ()
   "publish blog online"
   (interactive)
-    (start-process "my-process" "blog-export" "sh" "/Users/fuhongxue/.emacs.d/shell/blog-publish.sh" "/bin"))
+    (start-process "my-process" "automation" "sh" "/Users/fuhongxue/.emacs.d/shell/blog-publish.sh" "/bin"))
 
 (defun imi/backup-emacs ()
   "backup emacs configuration to github"
   (interactive)
-  (start-process "my-process" "blog-export" "sh" "/Users/fuhongxue/.emacs.d/shell/emacs-backup.sh" "/bin"))
+  (start-process "my-process" "automation" "sh" "/Users/fuhongxue/.emacs.d/shell/emacs-backup.sh" "/bin"))
 
 (provide 'init-blog)
