2ebcbd0dfaa5b386d555250c0ac60fa32ecdf050
imymirror <2633610394@qq.com>
2022-11-12 10:57:16 +0800

    backup

--- .vanilla-emacs.d/shell/emacs-backup.sh
+++ .vanilla-emacs.d/shell/emacs-backup.sh
@@ -7,13 +7,13 @@ pushd $IMYMIRROR_INSTALL_DIR
 IMY_EMACS_DIR=~/imymirror/project/imy-emacs.d
 VANILLA_DIR=$IMY_EMACS_DIR/.vanilla-emacs.d
 
-cp ./init.el $VANILLA_DIR
+cp ./init.el $VANILLA_DIR/
 
-cp -R ./shell $VANILLA_DIR 
-cp -R ./lisp $VANILLA_DIR
-cp -R ./snippets $VANILLA_DIR
+cp -R ./shell $VANILLA_DIR/ 
+cp -R ./lisp $VANILLA_DIR/
+cp -R ./snippets $VANILLA_DIR/
 
-cp -R ./site-lisp $VANILLA_DIR
+cp -R ./site-lisp $VANILLA_DIR/
 
 popd
 
